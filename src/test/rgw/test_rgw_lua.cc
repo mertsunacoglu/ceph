235b3fb6b9a22065af1c9714b15fabd98323c357
Emin Sunacoglu <emin.sunacoglu@clyso.com>
2025-12-11 09:27:11 +0100

    rgw: Add Bucket tags attribute for Lua scripts
    Expose bucket tags (from RGW_ATTR_TAGS) to the RGW Lua Request.Bucket.Tags table, allowing lua scripts to read these tags.
    
    Fixes: https://tracker.ceph.com/issues/74756
    
    Signed-off-by: Emin Sunacoglu <emin.sunacoglu@clyso.com>

--- src/test/rgw/test_rgw_lua.cc
+++ src/test/rgw/test_rgw_lua.cc
@@ -9,6 +9,8 @@
 #include "rgw_lua_background.h"
 #include "rgw_lua_data_filter.h"
 #include "rgw_sal_config.h"
+#include "rgw_perf_counters.h"
+#include "rgw_tag.h"
 
 using namespace std;
 using namespace rgw;
@@ -1638,6 +1640,35 @@ TEST(TestRGWLua, ReturnString)
   RGWBucketInfo info;
   info.bucket.tenant = "mytenant";
   info.bucket.name = "myname";
+  const auto rc = lua::request::execute(nullptr, nullptr, nullptr, &s, nullptr, script, return_code);
+  ASSERT_EQ(rc, 0);
+  EXPECT_NE(return_code, -EPERM);
+}
+
+TEST(TestRGWLua, NotValidLua)
+{
+  const std::string script = R"(
+  this is not valid lua code
+  )";
+
+  int return_code = 0;
+  DEFINE_REQ_STATE;
+  const auto rc = lua::request::execute(nullptr, nullptr, nullptr, &s, nullptr, script, return_code);
+  ASSERT_EQ(rc, -1);
+  EXPECT_NE(return_code, -EPERM);
+}
+
+TEST(TestRGWLua, BucketTags)
+{
+  const std::string script = R"(
+    assert(Request.Bucket.Tags["Project"] == "Ceph")
+  )";
+
+  DEFINE_REQ_STATE;
+  
+  RGWBucketInfo info;
+  info.bucket.tenant = "mytenant";
+  info.bucket.name = "myname";
   
   RGWObjTags tags;
   tags.add_tag("Project", "Ceph");
@@ -1648,6 +1679,54 @@ TEST(TestRGWLua, ReturnString)
   s.bucket_attrs[RGW_ATTR_TAGS] = bl;
   s.bucket.reset(new sal::RadosBucket(nullptr, info));
 
-const auto rc = lua::request::execute(nullptr, nullptr, nullptr, &s, nullptr, script);
-ASSERT_EQ(rc, 0);
+  const auto rc = lua::request::execute(nullptr, nullptr, nullptr, &s, nullptr, script);
+  ASSERT_EQ(rc, 0);
+}
+
+TEST(TestRGWLua, BucketTagsIteration)
+{
+  const std::string script = R"(
+    -- Test length operator
+    assert(#Request.Bucket.Tags == 3)
+    
+    local count = 0
+    local seen_project = false
+    local seen_owner = false
+
+    for k, v in pairs(Request.Bucket.Tags) do
+      count = count + 1
+      if k == "Project" then
+        assert(v == "Ceph")
+        seen_project = true
+      elseif k == "Owner" then
+        assert(v == "Ceph Team")
+        seen_owner = true
+      elseif k == "Status" then
+        assert(v == "Testing")
+      end
+    end
+
+    assert(count == 3, "Iterator count should be 3")
+    assert(seen_project == true, "Should have seen Project tag")
+    assert(seen_owner == true, "Should have seen Owner tag")
+  )";
+
+  DEFINE_REQ_STATE;
+  
+  RGWBucketInfo info;
+  info.bucket.name = "tag-test-bucket";
+  
+  RGWObjTags tags;
+  tags.add_tag("Project", "Ceph");
+  tags.add_tag("Owner", "Ceph Team");
+  tags.add_tag("Status", "Testing");
+  
+  bufferlist bl;
+  tags.encode(bl);
+
+  s.bucket_attrs[RGW_ATTR_TAGS] = bl;
+  s.bucket.reset(new sal::RadosBucket(nullptr, info));
+
+  const auto rc = lua::request::execute(nullptr, nullptr, nullptr, &s, nullptr, script);
+  ASSERT_EQ(rc, 0);
 }
